ecf898b2e21058a8c181d1011766eada94c1377b
clheard54 <clarion.heard@gmail.com>
2020-04-06 10:57:53 -0700

    Done.

--- src/components/Pig.js
+++ src/components/Pig.js
@@ -1,16 +1,24 @@
 import React from "react"
 import exclamation from "../assets/exclamation.png"
 
+let timer = null
 export default class Pig extends React.Component {
 
 
   panic = () => (<img className="exclamation" src={exclamation} alt="" />)
 
+  freakOut = () => {
+    clearInterval(timer);
+    if (this.props.environment == "inhospitable"){
+      this.panic()
+    }
+    timer = setTimeout(this.props.relax, 2000)
+  }
 
   render() {
     return(
       <div id={this.props.name} className="sheeple">
-        {null}
+      {this.freakOut()}
       </div>
     )
   }
